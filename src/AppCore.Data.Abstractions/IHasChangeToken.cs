40eed9e8dbc604692f8c8a4731f795224d01f296
Christian Prochnow <christian.prochnow77@gmail.com>
2020-09-23 16:45:55 +0200

    Update API docs.

--- src/AppCore.Data.Abstractions/IHasChangeToken.cs
+++ src/AppCore.Data.Abstractions/IHasChangeToken.cs
@@ -4,13 +4,17 @@
 namespace AppCore.Data
 {
     /// <summary>
-    /// Represents an entity with a concurrency token.
+    /// Represents an entity which implements optimistic concurrency checks with a automatically managed change token.
     /// </summary>
     public interface IHasChangeToken
     {
         /// <summary>
-        /// Gets the concurrency token.
+        /// Gets the current/expected concurrency token.
         /// </summary>
+        /// <remarks>
+        /// The property contains the current change token after the entity has been loaded.
+        /// After the entity has been saved the change token will have a updated value.
+        /// </remarks>
         string ChangeToken { get; }
     }
 }
