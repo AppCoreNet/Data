b468bcba34ed051a00098624b38d48f47a8a79e4
Christian Prochnow <christian.prochnow77@gmail.com>
2021-07-24 09:51:40 +0200

    Added methods to configure the DbContext.

--- src/AppCore.Data.EntityFrameworkCore/EntityFrameworkCoreFacilityExtension.cs
+++ src/AppCore.Data.EntityFrameworkCore/EntityFrameworkCoreFacilityExtension.cs
@@ -1,8 +1,11 @@
 // Licensed under the MIT License.
 // Copyright (c) 2020-2021 the AppCore .NET project.
 
+using System;
+using System.Collections.Generic;
 using AppCore.Data.EntityFrameworkCore;
 using AppCore.DependencyInjection.Facilities;
+using AppCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -18,6 +21,9 @@ namespace AppCore.Data
     public class EntityFrameworkCoreFacilityExtension<TTag, TDbContext> : FacilityExtension
         where TDbContext : DbContext
     {
+        private int? _poolSize;
+        private readonly List<Action<IServiceProvider, DbContextOptionsBuilder>> _options = new();
+
         /// <inheritdoc />
         protected override void ConfigureServices(IServiceCollection services)
         {
@@ -34,8 +40,45 @@ namespace AppCore.Data
                         sp => sp.GetRequiredService<IDbContextDataProvider<TDbContext>>()
                                 .TransactionManager)
                 });
+
+            if (_poolSize.HasValue)
+            {
+                services.AddDbContextPool<TDbContext>(o => { }, (int) _poolSize);
+
+                foreach (Action<IServiceProvider, DbContextOptionsBuilder> options in _options)
+                {
+                    services.AddDbContextPool<TDbContext>(options, (int)_poolSize);
+                }
+            }
+            else
+            {
+                services.AddDbContext<TDbContext>();
+
+                foreach (Action<IServiceProvider, DbContextOptionsBuilder> options in _options)
+                {
+                    services.AddDbContext<TDbContext>(options);
+                }
+            }
         }
-        
+
+        public EntityFrameworkCoreFacilityExtension<TTag, TDbContext> WithPoolSize(int? poolSize = 128)
+        {
+            _poolSize = poolSize;
+            return this;
+        }
+
+        public EntityFrameworkCoreFacilityExtension<TTag, TDbContext> WithOptions(Action<IServiceProvider, DbContextOptionsBuilder> options)
+        {
+            Ensure.Arg.NotNull(options, nameof(options));
+            _options.Add(options);
+            return this;
+        }
+
+        public EntityFrameworkCoreFacilityExtension<TTag, TDbContext> WithOptions(Action<DbContextOptionsBuilder> options)
+        {
+            return WithOptions((_, o) => options(o));
+        }
+
         public EntityFrameworkCoreFacilityExtension<TTag, TDbContext> WithRepository<TId, TEntity, TDbEntity>()
             where TEntity : IEntity<TId>
             where TDbEntity : class
